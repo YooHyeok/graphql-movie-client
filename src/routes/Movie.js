ee4a2c51aa5ff2f3459c2689386214e381d82bd2
webdevyoo <yjk7454@naver.com>
2024-02-09 17:39:39 +0900

    feat: useQuery의 variables를 적용하여 Movie 디테일 컴포넌트 구현

--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,3 +1,26 @@
+import { gql, useQuery } from "@apollo/client"
+import { useParams } from "react-router-dom"
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id:$movieId) {
+      id
+      title
+    }
+  }
+`
+
 export default function Movie() {
-  return <div>This Is Movie Detail</div>
+  const params = useParams()
+  const {data, loading, error} = useQuery(GET_MOVIE, {variables: {
+    movieId: params.id
+  }})
+  if(loading) {
+    return <h1>Fetcing movie....</h1>
+  }
+  if(error) {
+    console.log(error)
+    return <h1>Could not fetch :(</h1>
+  }
+  return <div>{data.movie.title}</div>
 }
